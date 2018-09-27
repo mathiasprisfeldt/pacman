4ee7a6dd8585c46a00bcd27d798b1897180b513e
Mathias Prisfeldt <contact@mathiasprisfeldt.me>
2018-09-27 23:37:45 +0200

    Added some stuff to main menu

--- app/src/main/java/me/mathiasprisfeldt/pacman/MainMenu.java
+++ app/src/main/java/me/mathiasprisfeldt/pacman/MainMenu.java
@@ -2,6 +2,7 @@ package me.mathiasprisfeldt.pacman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.EventListener;
 import java.util.EventListenerProxy;
